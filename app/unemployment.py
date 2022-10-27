4390b6177b4d99358ad842c67417bb1d8c1601f4
acsigi <ac2072@georgetown.edu>
2022-10-27 13:44:41 -0400

    challenge a, b, c included

--- app/unemployment.py
+++ app/unemployment.py
@@ -5,6 +5,10 @@ import requests
 import json
 from pprint import pprint
 
+from dotenv import load_dotenv 
+
+load_dotenv()
+
 
 API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 
@@ -23,5 +27,39 @@ pprint(parsed_response)
 
 #breakpoint()
 
-latest = parsed_response["data"][0]
+latest = parsed_response["data"][0]["value"]
+
+print("-------------------------")
+print("LATEST UNEMPLOYMENT RATE:")
 print(latest)
+
+
+# Challenge B
+# 
+# What is the average unemployment rate for all months during this calendar year?
+# ... How many months does this cover?
+
+from statistics import mean
+
+data = parsed_response["data"]
+
+this_year = [d for d in data if "2022-" in d["date"]]
+
+rates_this_year = [float(d["value"]) for d in this_year]
+#print(rates_this_year)
+
+print("-------------------------")
+print("AVG UNEMPLOYMENT THIS YEAR:", f"{mean(rates_this_year)}%")
+print("NO MONTHS:", len(this_year))
+
+# Challenge C
+# 
+# Plot a line chart of unemployment rates over time.
+
+from plotly.express import line
+
+dates = [d["date"] for d in data]
+rates = [float(d["value"]) for d in data]
+
+fig = line(x=dates, y=rates, title="United States Unemployment Rate over time", labels= {"x": "Month", "y": "Unemployment Rate"})
+fig.show()
